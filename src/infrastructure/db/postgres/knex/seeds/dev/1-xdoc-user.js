709fa4b7124805c1724a3fb1c329dca7e95bbe0d
het-t <hettarkhala068@gmail.com>
2024-08-31 18:15:34 +0530

    feat(seeder)
    for sign in functionality

---
+++ src/infrastructure/db/postgres/knex/seeds/dev/1-xdoc-user.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('xdoc_user').truncate()
+  await knex('xdoc_user').insert([
+    {
+      id: "d5a93cea-695e-4b76-a12f-d552cfbc606f", 
+      email: "johndoe@gmail.com",
+      name: "John Doe"
+    },
+    {
+      id: "c09448fd-6974-4aa5-8d96-9769434287f7", 
+      email: "angel@xdoc.com",
+      name: "Angel Heaven"
+    },
+    {
+      id: "f4bc614a-2dc2-48b9-9d76-0eff4d1f990a", 
+      email: 'jimmy@xdoc.com',
+      name: "Jimmy Alone"
+    }
+  ]);
+};
---
+++ src/infrastructure/db/postgres/knex/seeds/dev/xdoc_user_auth.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('xdoc_user_auth').truncate()
+  await knex('xdoc_user_auth').insert([
+    {
+      user_id: "d5a93cea-695e-4b76-a12f-d552cfbc606f", 
+      password: "john@123"
+    },
+    {
+      user_id: "c09448fd-6974-4aa5-8d96-9769434287f7", 
+      password: "angel@xdoc"
+    },
+    {
+      user_id: "f4bc614a-2dc2-48b9-9d76-0eff4d1f990a", 
+      password: 'JimmyJKPA'
+    }
+  ]);
+};
